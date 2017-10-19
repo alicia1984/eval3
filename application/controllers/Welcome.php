6a62c3360ddf9fbf289120374718c47eef5d1045
alicia1984 <alicia28031@hotmail.com>
2017-10-19 19:56:44 -0300

    crear funcion y variables para  formulario
    puntos  de la EVALUACION  4 Y 5

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,11 +6,24 @@ class Welcome extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->helper('form');
-		//$this->load->model('form_model');  
+		$this->load->model('form_model');  
 	}
 
 	public function index()
 	{
 		$this->load->view('formulario');
+	}
+	{
+		function validar()
+	{	
+		//echo "Hola<br>";
+		//aqui  estoy creando una funcion  para  el  controlador  para  guardar  datos recolectados 
+
+		$name = $this->input->post('phpalicia');
+		$lastname = $this->input->post('segundoapellido');
+		$DirecciondeEmail = $this->input->post('ingresacorreo');
+		echo "Nombre: ".$name."  Apellido: ".$lastname."correo" .$DirecciondeEmail;
+
+
 	}
 }
